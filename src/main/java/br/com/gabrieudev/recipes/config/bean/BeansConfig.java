5906782f90e0bd2ac5839dc312ee5a4c70ac1576
gabrieudev <gabrieudev@gmail.com>
2025-08-17 14:23:26 -0300

    chore: atualizar configurações de beans

--- src/main/java/br/com/gabrieudev/recipes/config/bean/BeansConfig.java
+++ src/main/java/br/com/gabrieudev/recipes/config/bean/BeansConfig.java
@@ -4,17 +4,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import br.com.gabrieudev.recipes.application.ports.input.AuthInputPort;
+import br.com.gabrieudev.recipes.application.ports.input.CategoryInputPort;
+import br.com.gabrieudev.recipes.application.ports.input.FavoriteInputPort;
+import br.com.gabrieudev.recipes.application.ports.input.IngredientInputPort;
+import br.com.gabrieudev.recipes.application.ports.input.RecipeinputPort;
 import br.com.gabrieudev.recipes.application.ports.input.RoleInputPort;
 import br.com.gabrieudev.recipes.application.ports.input.UserInputPort;
 import br.com.gabrieudev.recipes.application.ports.input.UserRoleInputPort;
 import br.com.gabrieudev.recipes.application.ports.output.AuthOutputPort;
 import br.com.gabrieudev.recipes.application.ports.output.CacheOutputPort;
+import br.com.gabrieudev.recipes.application.ports.output.CategoryOutputPort;
 import br.com.gabrieudev.recipes.application.ports.output.EmailOutputPort;
 import br.com.gabrieudev.recipes.application.ports.output.EnvironmentOutputPort;
+import br.com.gabrieudev.recipes.application.ports.output.FavoriteOutputPort;
+import br.com.gabrieudev.recipes.application.ports.output.IngredientOutputPort;
+import br.com.gabrieudev.recipes.application.ports.output.RecipeOutputPort;
 import br.com.gabrieudev.recipes.application.ports.output.RoleOutputPort;
 import br.com.gabrieudev.recipes.application.ports.output.UserOutputPort;
 import br.com.gabrieudev.recipes.application.ports.output.UserRoleOutputPort;
 import br.com.gabrieudev.recipes.application.services.AuthService;
+import br.com.gabrieudev.recipes.application.services.CategoryService;
+import br.com.gabrieudev.recipes.application.services.FavoriteService;
+import br.com.gabrieudev.recipes.application.services.IngredientService;
+import br.com.gabrieudev.recipes.application.services.RecipeService;
 import br.com.gabrieudev.recipes.application.services.RoleService;
 import br.com.gabrieudev.recipes.application.services.UserRoleService;
 import br.com.gabrieudev.recipes.application.services.UserService;
@@ -59,5 +71,45 @@ public class BeansConfig {
     @Bean
     UserRoleInputPort userRoleInputPort(UserRoleOutputPort userRoleOutputPort, RoleOutputPort roleOutputPort, UserOutputPort userOutputPort) {
         return new UserRoleService(userRoleOutputPort, roleOutputPort, userOutputPort);
+    }
+
+    @Bean
+    CategoryService categoryService(CategoryOutputPort categoryOutputPort) {
+        return new CategoryService(categoryOutputPort);
+    }
+
+    @Bean
+    CategoryInputPort categoryInputPort(CategoryOutputPort categoryOutputPort) {
+        return new CategoryService(categoryOutputPort);
+    }
+
+    @Bean
+    FavoriteService favoriteService(FavoriteOutputPort favoriteOutputPort) {
+        return new FavoriteService(favoriteOutputPort);
+    }
+
+    @Bean
+    FavoriteInputPort favoriteInputPort(FavoriteOutputPort favoriteOutputPort) {
+        return new FavoriteService(favoriteOutputPort);
+    }
+
+    @Bean
+    IngredientService ingredientService(IngredientOutputPort ingredientOutputPort) {
+        return new IngredientService(ingredientOutputPort);
+    }
+
+    @Bean
+    IngredientInputPort ingredientInputPort(IngredientOutputPort ingredientOutputPort) {
+        return new IngredientService(ingredientOutputPort);
+    }
+
+    @Bean
+    RecipeService recipeService(RecipeOutputPort recipeOutputPort) {
+        return new RecipeService(recipeOutputPort);
+    }
+
+    @Bean
+    RecipeinputPort recipeInputPort(RecipeOutputPort recipeOutputPort) {
+        return new RecipeService(recipeOutputPort);
     }
 }
